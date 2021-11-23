d098877aa50c02f77660b29572830524b0405cb7
Andre Manchanthasouk <dremanchan@gmail.com>
2021-11-22 23:37:22 -0600

    created addToCollection function and added 6 items

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,20 @@
 console.log('***** Music Collection *****')
 let collection = [];
 
+function addToCollection( title, yearPublished, artist ) {
+    collection.push(
+        title,
+        yearPublished,
+        artist
+    );
+}
+
+
+addToCollection('Modal Soul', 2005, 'Nujabes');
+addToCollection('Impression', 2004, 'Nujabes');
+addToCollection('Recto Verso', 2016, 'Paradis');
+addToCollection('Tuxedo', 2015, 'Tuxedo');
+addToCollection('Between Us', 2019, 'Hayden James');
+addToCollection('Lucid', 2019, 'Raveena');
+
+console.log(collection);
