873d3c411c4cd05ed95075f682a90e868cce3d59
Andre Manchanthasouk <dremanchan@gmail.com>
2021-11-23 01:17:58 -0600

    showCollection isn't working

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -18,3 +18,14 @@ addToCollection('Between Us', 2019, 'Hayden James');
 addToCollection('Lucid', 2019, 'Raveena');
 
 console.log(collection);
+
+function showCollection( title, yearPublished, artist ) {
+    for(i = 0; i < collection.length; i++){
+        console.log((title), ' by ',(artist), ' published in ', (yearPublished));
+    }
+    console.log(collection.length);
+    }
+
+showCollection('Between Us');
+showCollection(2016);
+showCollection('Nujabes');
